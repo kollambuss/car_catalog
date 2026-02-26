9151fd722aa3347f682e3e52551c846ea507b3fa
Chara <sajdusdart@gmail.com>
2026-02-26 22:00:31 +0300

    feat: add 7 more car records for analysis

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,4 +1,23 @@
-INSERT INTO cars (brand, model, year, color, price) VALUES
-('Toyota', 'Camry', 2020, 'Silver', 1500000.00),
-('BMW', 'X5', 2022, 'Black', 4500000.50),
-('Lada', 'Vesta', 2021, 'White', 800000.00);
+INSERT INTO cars (brand, model, year, color, price) VALUES 
+
+('Toyota', 'Camry', 2020, 'Silver', 1500000.00), 
+
+('BMW', 'X5', 2022, 'Black', 4500000.50), 
+
+('Lada', 'Vesta', 2021, 'White', 800000.00),
+
+('Ford', 'Mustang', 2020, 'Blue', 3000000.00),
+
+('Hyundai', 'Solaris', 2023, 'Red', 1103000.00),
+
+('Kia', 'Rio', 2021, 'Gray', 955020.00),
+
+('Mercedes', 'E-Class', 2022, 'Black', 5500007.00),
+
+('Audi', 'A4', 2023, 'White', 4200300.00),
+
+('Volkswagen', 'Polo', 2025, 'Green', 850000.00),
+
+('Nissan', 'N5', 2022, 'Blue', 1810000.00),
+
+('BMW', 'XM', 2024, 'Black', 3202000.00);
